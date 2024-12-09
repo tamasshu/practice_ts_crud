2e5299dc1882ae2b006f16072065dddd76f3acc6
naoko.tamagaki <166595834+tamasshu@users.noreply.github.com>
2024-12-09 18:43:22 +0900

    設定ファイルの修正・TaskPropsの微修正

--- src/types/TaskProps.ts
+++ src/types/TaskProps.ts
@@ -1,13 +1,11 @@
 export type TaskProps = {
-  tasks: [
-    {
-      id: number;
-      title: string;
-      assignedPokemon: string;
-      assignedPokemonImage?: string;
-    }
-  ];
+  tasks: {
+    id: number;
+    title: string;
+    assignedPokemon: string;
+    assignedPokemonImage?: string;
+  };
   deleteTask: (id: number) => void;
 };
 
-export type Task = TaskProps["tasks"][number];
+export type Task = TaskProps["tasks"];
